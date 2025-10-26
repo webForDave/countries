db8e0d3e54b2cab9c05d99edefefa80425b0df09
David Akinola <dakinola54@gmail.com>
2025-10-26 16:38:56 +0100

    Appication registration and database model template

--- countries/models.py
+++ countries/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=256, null=False)
+    capital = models.CharField(max_length=256, null=True)
+    region = models.CharField(max_length=256, null=True)
+    population = models.IntegerField(default=0, null=False)
+    currency_code = models.CharField(max_length=5, null=True)
+    exchange_rate = models.FloatField(null=True)
+    estimated_gdp = models.FloatField(default=0, null=True)
+    flag_url = models.CharField(max_length=512, null=True)
+    last_refreshed_at = models.DateTimeField(null=False)
