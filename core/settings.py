260b0050fc8f6a83d25cbd8bb0d03c49b0a5c31f
David Akinola <dakinola54@gmail.com>
2025-10-27 18:11:43 +0100

    Properly load env variables

--- core/settings.py
+++ core/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
